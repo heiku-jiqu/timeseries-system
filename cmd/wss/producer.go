185016c46c2a434851a8e2d9ae36d4ad757bc5e3
Heiser <heiser9295@hotmail.com>
2023-11-10 12:15:50 +0800

    update producer

--- cmd/wss/producer.go
+++ cmd/wss/producer.go
@@ -10,36 +10,40 @@ import (
 
 // Creates new Producer. Caller needs to Flush() and Close() the Producer when done using.
 func NewKafkaProducer() *kafka.Producer {
-	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost:9092"})
+	cfg := &kafka.ConfigMap{
+		"bootstrap.servers": "localhost:9092",
+		"client.id":         "coinbase-ticker-app",
+	}
+	p, err := kafka.NewProducer(cfg)
 	if err != nil {
 		log.Fatal(err)
 	}
 	return p
 }
 
+// Logs Producer's message delivery
+// Run as goroutine to not block
 func deliveryReporter(p *kafka.Producer) {
-	go func() {
-		for e := range p.Events() {
-			switch ev := e.(type) {
-			case *kafka.Message:
-				if ev.TopicPartition.Error != nil {
-					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
-				} else {
-					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
-				}
+	for e := range p.Events() {
+		switch ev := e.(type) {
+		case *kafka.Message:
+			if ev.TopicPartition.Error != nil {
+				fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
+			} else {
+				fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
 			}
 		}
-	}()
+	}
 }
 
 func produceMessage(p *kafka.Producer, t Ticker) {
-	topic := "Cryptocurrency"
+	topic := "coinbase-ticker"
 	val, err := json.Marshal(t)
 	if err != nil {
 		log.Fatal(err)
 	}
 	p.Produce(&kafka.Message{
-		TopicPartition: kafka.TopicPartition{Topic: &topic},
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
 		Key:            []byte(t.ProductID),
 		Value:          val,
 	}, nil)
